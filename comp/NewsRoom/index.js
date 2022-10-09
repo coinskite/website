e0c0e07d5db56c721610eb862bf5a34f36b1fd16
Raj kumar <rajkumar@inclusivegrowthchain.com>
2022-10-09 15:06:07 +0530

    newsroom added

---
+++ comp/NewsRoom/index.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function NewsRoom() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default NewsRoom
---
+++ pages/newsroom.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function newsroom() {
+  return (
+    <div>newsroom</div>
+  )
+}
+
+export default newsroom
