95f15d7a3d9e1cb03ecab0a497af2c9e6f6d4cbb
Raj kumar <rajkumar@inclusivegrowthchain.com>
2022-10-09 15:39:48 +0530

    solutions page added

--- comp/Solutions/Header.js
+++ comp/Solutions/Header.js
@@ -1,11 +1,13 @@
 function Header() {
   return (
-    <div className='df theme-sec theme-header solution-header pad-main-3'>
-      <div className='dfc gap1'>
-        <h1>Blockchain Solutions by Coinskite</h1>
-        <p>The adoption and deployment of Ethereum blockchain solutions by financial institutions, crypto-enterprises, and world governments is in full swing. The Coinskite product suite leverages blockchain to address long-standing industry problems across traditional finance, decentralized finance, commerce, and trade. Find out how.</p>
-        <p><button className='br5 black-bg'>Connect with our experts</button></p>
-      </div>
+    <div className='dfc justify-center theme-sec pad-main-3 h-[30vh] sm:h-[40vh] md:h-[45vh] lg:h-[55vh] xl:h-[65vh] text-[#223764] text-center'>
+      <h5 className="text-[13px] md:text-[15px] lg:text-[25px] xl:text-[35px] font-bold">Solutions</h5>
+      <h1 className="text-xl sm:text-[25px] md:text-[28px] lg:text-[40px] xl:text-6xl font-bold">Blockchain Solutions By Coinskite</h1>
+      <p className="text-xs sm:text-[15px] md:text-lg lg:text-[22px] xl:text-[26px] font-medium">
+        Want to be a part of building next-gen frameworks and products <br />
+        using blockchain, DLT, and other peer-to-peer technologies <br />
+        while collaborating with a global team.
+      </p>
     </div>
   )
 }
--- comp/Solutions/Main.js
+++ comp/Solutions/Main.js
@@ -1,60 +1,87 @@
-function Sect({ title, para }) {
+const data = [
+  {
+    title: "FINANCE",
+    para: "Payments and money transfers made on the blockchain are faster and more traceable than in traditional bankingWhen information flows through different financial intermediaries, there is a risk of interception of that information, raising the possibility of fraud.",
+    img: "./img/solution/Finance.png",
+  },
+  {
+    title: "INSURANCE",
+    para: "Blockchain has the ability to help automate claims functions by verifying coverage between companies and reinsurers It will also automate payments between parties for claims and thus lower administrative costs for insurance companies.",
+    img: "./img/solution/Insurance.png",
+  },
+  {
+    title: "REAL ESTATE",
+    para: "Blockchain technology has impacted the real estate industry in a variety of ways,including offering a new means for buyers and sellers to connect with one another.Blockchain could be used to cut intermediaries out of the real estate transaction process, thereby reducing costs.",
+    img: "./img/solution/Real estate.png",
+  },
+  {
+    title: "MEDIA",
+    para: "The decentralized structure of blockchain could enable content creators—such as musicians or writers—to directly distribute their work to consumers, by passing traditional distribution channels and leaving a larger share of revenue for content creators themselves",
+    img: "./img/solution/Media.png",
+  },
+  {
+    title: "SUPPLY CHAIN",
+    para: "Blockchain can enable more transparent and accurate end-to-end tracking in the supply chain: Organizations can digitize physical assets and create a decentralized immutable record of all transactions, making it possible to track assets from production to delivery or use by end user.",
+    img: "./img/solution/Supplychain.png",
+  },
+  {
+    title: "LAW",
+    para: "Blockchain may change how law firms conduct a multitude of services. Everything from escrow management to contract and transactional processes are already adapting and leveraging these tools, including: Automated Settlements of Security Transactions",
+    img: "./img/solution/Law.png",
+  },
+  {
+    title: "SPORTS",
+    para: "Blockchain-based games can be programmed with tokenized digital assets. These digital assets can be traded within a game or externally on the public market. Blockchain technology can also create real-time sports betting with provably fair outcomes.",
+    img: "./img/solution/Sports.png",
+  },
+  {
+    title: "HEALTHCARE",
+    para: "Blockchain has a wide range of applications and uses in healthcare. The ledger technology facilitates the secure transfer of patient medical records, manages the medicine supply chain and helps healthcare researchers unlock genetic code.",
+    img: "./img/solution/Healthcare.png",
+  },
+  {
+    title: "GOVERNMENT",
+    para: "By capturing votes as transactions through a blockchain, governments and voters would have a verifiable audit trail, ensuring no votes are changed or removed and no illegitimate votes are added",
+    img: "./img/solution/Government.png",
+  },
+  {
+    title: "SOCIAL MEDIA",
+    para: "How does Blockchain technology affect society? With its tracked, audited, and publicly communicated information, blockchain may be able to rebuild those bridges between centralized systems and the people they serve. Verifiable timestamps, geolocations, and supply-chain records can help citizens decipher the truth.",
+    img: "./img/solution/Social media.png",
+  }
+]
+
+function Sect({ title, para, img }) {
   return (
-    <div className='sol-main-grid'>
-      <p className='theme-bg'></p>
-      <h5>{title}</h5>
-      <p>{para}</p>
+    <div className='dfc items-center p-4 md:p-6 text-[#F5F5F5] border border-[#22C954] bg-[#0F161B] rounded-lg'>
+      <img className="h-20 md:h-24" src={img} alt={title} />
+      <h6 className="relative my-2 text-[15px] sm:text-[17px] md:text-[19px] lg:text-[25px] xl:text-3xl font-extrabold">
+        {title}
+        <span className="h-px w-full absolute bottom-0 left-0 bg-[#22C954]"></span>
+      </h6>
+      <p className="mt-2 mb-4 text-[6px] sm:text-[8px] lg:text-[10px] xl:text-xs font-semibold">{para}</p>
+      <button className="mt-auto py-2 px-8 mx-auto text-[7.6px] md:text-[8px] lg:text-[10px] font-semibold text-white bg-[#22C954] rounded-full">
+        Learn more {`>`}
+      </button>
     </div>
   )
 }
 
 function Main() {
   return (
-    <div className='pad-main-3 solution-main'>
-      <Sect
-        title="Finance"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going"
-      />
-      <Sect
-        title="Insurance"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going"
-      />
-      <Sect
-        title="Real Estate"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going "
-      />
-      <Sect
-        title="Law"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going "
-      />
-
-      <Sect
-        title="Social media"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going "
-      />
-      <Sect
-        title="Healthcare"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going "
-      />
-      <Sect
-        title="Media"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going "
-      />
-      <Sect
-        title="Sports"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going "
-      />
-
-      <div></div>
-      <Sect
-        title="Supplychain"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going "
-      />
-      <Sect
-        title="Government"
-        para="Altoros assists enterprises at each step of a development life cycle: from ideation to and going "
-      />
-      <div></div>
+    <div className='pad-main-3 text-white bg-[#222531]'>
+      <div className="grid grid-cols-2 gap-4 sm:gap-8 md:gap-12 lg:gap-16 xl:gap-20 max-w-sm sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl mx-auto">
+        {
+          data.map(d => (
+            <Sect
+              key={d.title}
+              title={d.title}
+              para={d.para}
+              img={d.img}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
