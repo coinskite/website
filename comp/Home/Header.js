ee52e24966ffe6703bfbab0c40fc39c6b25adcde
Raj kumar <rajkumar@inclusivegrowthchain.com>
2022-10-26 08:20:17 +0530

    header images added

--- comp/Home/Header.js
+++ comp/Home/Header.js
@@ -1,16 +1,17 @@
 function Header() {
   return (
-    <div className="dfc home-header-bg jcc gap-2.5 md:gap-8 pad-main-1-3 h-[40vh] sm:h-[60vh] md:h-[80vh] relative bg-cover">
-      <h1 className="text-lg sm:text-[27px] md:text-[35px] lg:text-[50px] font-bold">
+    <div className="dfc jcc gap-2.5 md:gap-8 pad-main-1-3 h-[40vh] sm:h-[60vh] md:h-[80vh] relative bg-cover bg-primary-900">
+      <h1 className="text-lg sm:text-[27px] md:text-[35px] lg:text-[50px] font-bold z-[1]">
         We build solution on the
         <br />
-        Blockchain      </h1>
-      <p className="text-[7.5px] sm:text-[13px] md:text-lg lg:text-[25px]">
+        Blockchain
+      </h1>
+      <p className="text-[7.5px] sm:text-[13px] md:text-lg lg:text-[25px] z-[1]">
         Building and deploying custom blockchain solutions for high-growth
         <br />
         startups and enterprises.
       </p>
-      <p className="df gap-4">
+      <p className="df gap-4 z-[1]">
         <button className="w-16 h-[18px] lg:w-44 lg:h-12 text-[7px] lg:text-xl font-semibold text-[#17171F] bg-white rounded-[3px]">
           Services
         </button>
@@ -19,11 +20,37 @@ function Header() {
         </button>
       </p>
 
-      {/* <img
-        className="hidden xs:block xs:w-44 sm:w-60 md:w-96 xl:w-auto xs:absolute xs:top-1/2 xs:translate-y-[-45%] sm:-translate-y-1/2 right-4 lg:right-8"
-        src="./img/home/HeaderBox.png"
-        alt="box"
-      /> */}
+      <img
+        className="absolute top-1/4 right-1/4"
+        src="./img/home/header/1.png"
+        alt=""
+      />
+      <img
+        className="absolute top-1/2 right-[10%]"
+        src="./img/home/header/1.png"
+        alt=""
+      />
+      <img
+        className="absolute top-3/4 right-1/4"
+        src="./img/home/header/1.png"
+        alt=""
+      />
+
+      <img
+        className="absolute -left-20 top-1/2 -translate-y-1/2 w-40"
+        src="./img/home/header/Left middle.png"
+        alt=""
+      />
+      <img
+        className="hidden xl:block absolute top-0 right-0 w-40"
+        src="./img/home/header/Right TOP.png"
+        alt=""
+      />
+      <img
+        className="hidden xl:block absolute -bottom-20 right-0 w-44"
+        src="./img/home/header/Right Bottom.png"
+        alt=""
+      />
     </div>
   )
 }
