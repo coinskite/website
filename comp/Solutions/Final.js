7a21deca8738271f87b4e1b162f0dd288f7686f0
mkausar <mohammedkawuser20@gmail.com>
2023-02-05 13:48:51 +0530

    final button

--- comp/Solutions/Final.js
+++ comp/Solutions/Final.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Final() {
   return (
     <div className='df pad-main-3 columns-{xs} bg-[#CAFCD9]'>
-      <p className='w-[900px] text-[#223764] text-[8px] xs:text-[10px] sm:text-sm md:text-[18px] lg:text-2xl xl:text-[28px] font-semibold'>Explore the possibilities of blockchain with Coinskite</p>
-      <button className='bg-[#22C954] text-white ml-40 text-[10px] xs:text-xs sm:text-base md:text-lg lg:text-[25px] font-extralight '>
+      <p className='xl:w-[750px]  text-[#223764] text-[8px] xs:text-[10px] sm:text-sm md:text-[18px] lg:text-2xl xl:text-[28px] font-semibold'>Explore the possibilities of blockchain with Coinskite</p>
+      <button className=' bg-[#22C954] ml-40 text-white  text-[10px] xs:text-xs sm:text-base md:text-lg lg:text-[25px] font-extralight '>
         Contact Us
       </button>
     </div>
