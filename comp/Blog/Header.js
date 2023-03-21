31f4425f4c37c30e3339e9e4ddc5c4bd4f6672ae
arunjustin <arunjustin45@gmail.com>
2023-03-21 21:01:12 +0530

    font correction addded - Blog

--- comp/Blog/Header.js
+++ comp/Blog/Header.js
@@ -6,26 +6,26 @@ function Header() {
       </div>
 
       <div>
-        <button className="text-white bg-primary-900">
+        <button className="ts:text-[12px] xs:text-[12px] sm:text-[12px] font-semibold text-white bg-primary-900">
           NEW
         </button>
 
-        <p>
+        <p className="ts:text[14px] xs:text-[16px] sm:text-[18px] lg:text-[22px] xl:text-[25px] font-semibold">
           Map2Earn to help merge physical and
           virtual worlds
         </p>
 
-        <p>
+        <p className="ts:[8px] xs:text-[10px] md:text-[12px] lg:text-[14px] xl:text-[15px] font-medium ">
           Map2Earn is a revolutionary Metaverse-oriented economic model that
           enables users to become mappers while earning tokens
         </p>
 
         <p className="df">
-          <span>
+          <span className="ts:text[8px]  xs:text-[10px] sm:text-[10px] lg:text-[12px] xl:text-[15px] font-semibold">
             Rubesh,Researcher @coinskite
           </span>
 
-          <span>
+          <span className="ts:text[6px] xs:text-[6px] font-semibold">
             Jan 21,2023
           </span>
         </p>
