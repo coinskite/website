f29c3e2780db17ac7a7f6efaa8192cf179325016
Raj kumar <raj@coinskite.com>
2023-04-16 17:49:11 +0530

    resourse page finished

--- comp/Resource/index.js
+++ comp/Resource/index.js
@@ -1,58 +1,60 @@
+import { useState } from "react"
+
 const list = [
   {
     key: "1",
     src: "img/ebook/Metaverse.png",
-    subtitle:"The Metaverse Economy :",
-    title: " How Blockchain and Virtual Reality are Revolutionizing Business",
+    title: "The Metaverse Economy",
+    para: " How Blockchain and Virtual Reality are Revolutionizing Business",
     link: "",
   },
   {
     key: "2",
     src: "img/ebook/web3.png",
-    subtitle:"Web3.0:",
-    title: "The Future of the Internet and How Blockchain is Driving It",
+    title: "Web3.0:",
+    para: "The Future of the Internet and How Blockchain is Driving It",
     link: "",
   },
   {
     key: "3",
     src: "img/ebook/crypto-trading.png",
-    subtitle:"Crypto Trading Strategies:",
-    title: " Maximizing Profits and Minimizing Risks",
+    title: "Crypto Trading Strategies",
+    para: " Maximizing Profits and Minimizing Risks",
     link: "",
   },
   {
     key: "4",
     src: "img/ebook/Bitcoin.png",
-    subtitle:"Bitcoin and Beyond:",
-    title: " The Rise of Digital Currency and its Impact on Society",
+    title: "Bitcoin and Beyond",
+    para: " The Rise of Digital Currency and its Impact on Society",
     link: "",
   },
   {
     key: "5",
     src: "img/ebook/Landscape.png",
-    subtitle:"The Legal Landscape of Cryptocurrency:",
-    title: " Navigating Regulations and Compliance",
+    title: "The Legal Landscape of Cryptocurrency",
+    para: " Navigating Regulations and Compliance",
     link: "",
   },
   {
     key: "6",
     src: "img/ebook/Blockchain.png",
-    subtitle:"Blockchain for Dummies:",
-    title: "A Comprehensive Guide to Understanding Distributed Ledger Technology",
+    title: "Blockchain for Dummies",
+    para: "A Comprehensive Guide to Understanding Distributed Ledger Technology",
     link: "",
   },
   {
     key: "7",
     src: "img/ebook/marketing.png",
-    subtitle:"Metaverse Marketing:",
-    title: "How Brands Can Succeed in Virtual Worlds",
+    title: "Metaverse Marketing",
+    para: "How Brands Can Succeed in Virtual Worlds",
     link: "",
   },
   {
     key: "8",
     src: "img/ebook/game.png",
-    subtitle: "The Future of Gaming:",
-    title: "How Blockchain and NFTs are Disrupting the Video Game Industry",
+    title: "The Future of Gaming",
+    para: "How Blockchain and NFTs are Disrupting the Video Game Industry",
     link: "",
   },
 ]
@@ -66,35 +68,51 @@ const btns = [
   "Research",
 ]
 
-
 function Resource() {
+  const [selected, setSelected] = useState("All")
+
   return (
     <div className="pad-main-3">
-      <h1 className="text-xl ts:font-semibold xs:text-[22px] sm:text-3xl lg:text-[26px] xl:text-[40px] xl:font-semibold font-bold text-[#001377] ">Resource</h1>
-      <div>
-         {btns.map(b => (
-          <button key={b} className="text-base sm:text-xl lg:text-sm font-semibold text-[#001377] rounded-sm hover:bg-[#22C954]">
-            {b}
-          </button>
-         ))}
-      </div>
-      <div className=" lg:grid lg:grid-cols-2 gap-4">
-      {
-        list.map(l => (
-          <div key={l.key}>
-            <img src={l.src} 
-            alt=""
-            className=" h-[372px] xs:h-[460px] sm:h-[595px] lg:h-[600px]"
-            />
-            <p className="text-sm xs:text-lg sm:text-xl xl:font-normal xl:text-[22px]  text-[#001377] font-medium">{l.subtitle}</p>
-            <p className="text-sm xs:text-base font-normal sm:text-lg xl:text-xl text-[#001377] ">{l.title}</p>
-            <a href="/" className="text-sm xs:text-lg sm:text-xl xl:text-[22px] font-medium text-[#001168] underline">
-              {/* {l.link} */}
-              Get the book
-            </a>
-          </div>
-        ))
-      }
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-xl xs:text-[22px] sm:text-3xl lg:text-[26px] xl:text-[40px] font-semibold text-[#001377]">Resource</h1>
+
+        <div className="df no-scroll-bar my-4 overflow-x-auto">
+          {
+            btns.map(b => (
+              <button
+                key={b}
+                className={`shrink-0 px-3 py-0.5 text-base sm:text-xl lg:text-sm font-semibold text-[#001377] rounded-sm hover:bg-[#22C954] ${selected === b ? "bg-[#22C954]" : ""}`}
+                onClick={() => setSelected(b)}
+              >
+                {b}
+              </button>
+            ))
+          }
+        </div>
+
+        <div className="grid lg:grid-cols-2 gap-8 max-w-[254px] xs:max-w-[350px] sm:max-w-[420px] lg:max-w-4xl xl:max-w-5xl mx-auto">
+          {
+            list.map(l => (
+              <div key={l.key}>
+                <img
+                  className="h-[372px] xs:h-[460px] sm:h-[595px]"
+                  src={l.src}
+                  alt=""
+                />
+                <p className="mt-2 mb-1 text-sm xs:text-base font-normal sm:text-lg xl:text-xl text-[#001377]">
+                  <strong className="text-sm xs:text-lg sm:text-xl xl:font-normal xl:text-[22px] font-medium">
+                    {l.title} :
+                  </strong>
+                  {l.para}
+                </p>
+
+                <a href="/" className="text-sm xs:text-lg sm:text-xl xl:text-[22px] font-medium text-[#001168] underline">
+                  Get the book
+                </a>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
