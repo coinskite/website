6a354b1a39c51e6f23f77d37e75233752ef0205d
arunjustin <arunjustin45@gmail.com>
2023-05-28 12:52:17 +0530

    final-fontupdate

--- comp/Home/Final.js
+++ comp/Home/Final.js
@@ -4,11 +4,11 @@ function Final() {
     <div className="pad-main-3 relative">
       <div className='df justify-between px-3 py-5 md:p-6 max-w-5xl mx-auto bg-[#BFFFD3] rounded-[10px]'>
         <div>
-          <p className='font-bold text-[#18191F]'>
+          <p className='text-xs xs:text-lg sm:text-[22px] md:text-[26px] lg:text-4xl xl:text-[40px]  font-bold text-[#18191F]'>
             Become a member of this growing communtity
           </p>
 
-          <button className='df py-2 rounded-lg bg-primary-900'>
+          <button className='text-[6px] xs:text-[8px] sm:text-[10px] md:text-sm font-semibold df py-2 rounded-lg bg-primary-900'>
             Coinskite on Discord
             <img src="/img/home_new/community/discord.png" alt="" />
           </button>
