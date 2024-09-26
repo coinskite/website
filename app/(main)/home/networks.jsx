ee89d8c47aec9a9443826a9ce27f49a68ad2f6ff
RUBYNATHAN N <rubynathan999@gmail.com>
2024-09-26 12:35:57 +0530

    network-responsive

--- app/(main)/home/networks.jsx
+++ app/(main)/home/networks.jsx
@@ -41,21 +41,21 @@ function Networks() {
     <div className="py-4 px-4 sm:px-6 lg:px-8">
 
       <div className="flex items-center justify-center lg:justify-start">
-        <h2 className='mb-4 mt-10 text-xs sm:text-sm md:text-lg lg:text-2xl xl:text-3xl font-bold uppercase  bg-[#FFEF16] p-2 text-black'>
+        <h2 className='mb-4 mt-10 text-[8px] xs:text-[12px] sm:text-[16px] md:text-[16px] lg:text-[18px] xl:text-[28px] font-bold uppercase  bg-[#FFEF16] p-2 text-black'>
           AVAILABLE ON +10 NETWORKS
         </h2>
       </div>
 
       <div className="py-6 lg:py-8">
-        <div className="grid grid-cols-2 md:grid-cols-7 gap-4 md:gap-6 lg:gap-8 max-w-7xl mx-auto">
+        <div className="flex justify-between items-center gap-4 md:gap-6 lg:gap-8 ">
           {list.map(l => (
             <div key={l.id} className="flex flex-col items-center justify-center">
               <img
                 src={l.src}
                 alt={l.title}
-                className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 object-contain mb-2"
+                className="w-[30px] h-[27px] xs:w-[38px] xs:h-[35px] sm:w-[40px] sm:h-[38px] md:w-[50px] md:h-[48px] lg:w-[65px] lg:h-[64px]  xl:w-[75px] xl:h-[75px] object-contain mb-2"
               />
-              <span className="font-medium text-xs sm:text-sm md:text-base text-center">{l.title}</span>
+              <span className="h-[10px] font-medium text-[6px] xs:text-[8px] sm:text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px] text-center">{l.title}</span>
             </div>
           ))}
         </div>
